1c63853652e304ed6fd2a4046e91dda486e25b44
Evan Hahn <me@evanhahn.com>
2014-10-28 10:52:46 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Frameguard
+
+**Trying to prevent:** Your page being put in a `<frame>` or `<iframe>` without your consent. This helps to prevent things like [clickjacking attacks](https://en.wikipedia.org/wiki/Clickjacking).
+
+**How to we mitigate this:** The `X-Frame-Options` HTTP header restricts who can put your site in a frame. It has three modes: `DENY`, `SAMEORIGIN`, and `ALLOW-FROM`. If your app does not need to be framed (and most don't) you can use the default `DENY`. If your site can be in frames from the same origin, you can set it to `SAMEORIGIN`. If you want to allow it from a specific URL, you can allow that with `ALLOW-FROM` and a URL.
+
+Usage:
+
+```javascript
+var frameguard = require('frameguard');
+
+// These are equivalent:
+app.use(frameguard());
+app.use(frameguard('deny'));
+
+// Only let me be framed by people of the same origin:
+app.use(frameguard('sameorigin'));
+
+// Allow from a specific host:
+app.use(frameguard('allow-from', 'http://example.com'));
+```
+
+**Limitations:** This has pretty good (but not 100%) browser support: IE8+, Opera 10.50+, Safari 4+, Chrome 4.1+, and Firefox 3.6.9+. It only prevents against a certain class of attack, but does so pretty well. It also prevents your site from being framed, which you might want for legitimate reasons.
---
+++ test/index.js
@@ -0,0 +1,133 @@
+var frameguard = require('..');
+
+var connect = require('connect');
+var request = require('supertest');
+var assert = require('assert');
+
+var hello = function (req, res) {
+  res.end('Hello world!');
+};
+
+describe('frameguard', function () {
+
+  var app;
+  beforeEach(function () {
+    app = connect();
+  });
+
+  describe('with proper input', function () {
+
+    it('sets header to DENY with no arguments', function (done) {
+      app.use(frameguard()).use(hello);
+      request(app).get('/')
+      .expect('X-Frame-Options', 'DENY', done);
+    });
+
+    it('sets header to DENY when called with lowercase "deny"', function (done) {
+      app.use(frameguard('deny')).use(hello);
+      request(app).get('/')
+      .expect('X-Frame-Options', 'DENY', done);
+    });
+
+    it('sets header to DENY when called with uppercase "DENY"', function (done) {
+      app.use(frameguard('DENY')).use(hello);
+      request(app).get('/')
+      .expect('X-Frame-Options', 'DENY', done);
+    });
+
+    it('sets header to SAMEORIGIN when called with lowercase "sameorigin"', function (done) {
+      app.use(frameguard('sameorigin')).use(hello);
+      request(app).get('/')
+      .expect('X-Frame-Options', 'SAMEORIGIN', done);
+    });
+
+    it('sets header to SAMEORIGIN when called with uppercase "SAMEORIGIN"', function (done) {
+      app.use(frameguard('SAMEORIGIN')).use(hello);
+      request(app).get('/')
+      .expect('X-Frame-Options', 'SAMEORIGIN', done);
+    });
+
+    it('sets header properly when called with lowercase "allow-from"', function (done) {
+      app.use(frameguard('allow-from', 'http://example.com')).use(hello);
+      request(app).get('/')
+      .expect('X-Frame-Options', 'ALLOW-FROM http://example.com', done);
+    });
+
+    it('sets header properly when called with uppercase "ALLOW-FROM"', function (done) {
+      app.use(frameguard('ALLOW-FROM', 'http://example.com')).use(hello);
+      request(app).get('/')
+      .expect('X-Frame-Options', 'ALLOW-FROM http://example.com', done);
+    });
+
+    it('sets header properly when called with lowercase "allowfrom"', function (done) {
+      app.use(frameguard('allowfrom', 'http://example.com')).use(hello);
+      request(app).get('/')
+      .expect('X-Frame-Options', 'ALLOW-FROM http://example.com', done);
+    });
+
+    it('sets header properly when called with uppercase "ALLOWFROM"', function (done) {
+      app.use(frameguard('ALLOWFROM', 'http://example.com')).use(hello);
+      request(app).get('/')
+      .expect('X-Frame-Options', 'ALLOW-FROM http://example.com', done);
+    });
+
+    it('works with String object set to "SAMEORIGIN" and doesn\'t change them', function (done) {
+      /* jshint -W053 */
+      var str = new String('SAMEORIGIN');
+      /* jshint +W053 */
+      app.use(frameguard(str)).use(hello);
+      request(app).get('/')
+      .expect('X-Frame-Options', 'SAMEORIGIN', done);
+      assert.equal(str, 'SAMEORIGIN');
+    });
+
+    it('works with ALLOW-FROM with String objects and doesn\'t change them', function (done) {
+      /* jshint -W053 */
+      var directive = new String('ALLOW-FROM');
+      var url = new String('http://example.com');
+      /* jshint +W053 */
+      app.use(frameguard(directive, url));
+      app.use(hello);
+      request(app).get('/')
+      .expect('X-Frame-Options', 'ALLOW-FROM http://example.com', done);
+      assert.equal(directive, 'ALLOW-FROM');
+      assert.equal(url, 'http://example.com');
+    });
+
+  });
+
+  describe('with improper input', function () {
+
+    function callWith() {
+      var args = arguments;
+      return function () {
+        return frameguard.apply(helmet, args);
+      };
+    }
+
+    it('fails with a bad first argument', function () {
+      assert.throws(callWith(' '));
+      assert.throws(callWith('denyy'));
+      assert.throws(callWith('DENNY'));
+      assert.throws(callWith(' deny '));
+      assert.throws(callWith(' DENY '));
+      assert.throws(callWith(123));
+      assert.throws(callWith(false));
+      assert.throws(callWith(null));
+      assert.throws(callWith({}));
+      assert.throws(callWith([]));
+      assert.throws(callWith(['ALLOW-FROM', 'http://example.com']));
+      assert.throws(callWith(/cool_regex/g));
+    });
+
+    it('fails with a bad second argument if the first is "ALLOW-FROM"', function () {
+      assert.throws(callWith('ALLOW-FROM'));
+      assert.throws(callWith('ALLOW-FROM', null));
+      assert.throws(callWith('ALLOW-FROM', false));
+      assert.throws(callWith('ALLOW-FROM', 123));
+      assert.throws(callWith('ALLOW-FROM', ['http://website.com', 'http//otherwebsite.com']));
+    });
+
+  });
+
+});
